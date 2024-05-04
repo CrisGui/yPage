e91e52f3c8c496a735a909c63709ff764fce0268
CrisGui <159936410+CrisGui@users.noreply.github.com>
2024-05-04 14:06:47 +0100

    feat: add rest api methods
    - Add get method:
      * "/person" returns all persons
      * "/person/:id" returns person by id
    - Add post method:
      * "/person" create a new person
    - Add patch method:
      * "/person/:id" update a person by id

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 import express from "express"
 import cors from "cors"
+import datejs from "datejs"
 
-import { validatePerson, validatePartialPerson } from "./scheme/validations"
-import users from "../tests/MOCK_DATA.json"
+import { validatePerson, validatePartialPerson } from "./scheme/validations.js"
+import users from "../tests/MOCK_DATA.json" assert {type: "json"}
 
 const app = express()
 
@@ -27,6 +28,42 @@ const corsOptions = {
 
 app.use(express.json())
 app.use(cors(corsOptions))
+
+app.get("/person", (req, res) => {
+  res.status(200).json(users)
+})
+app.get("/person/:id", (req, res) => {
+  const { id } = req.params
+  const personIndex = users.findIndex(user => user.id === id)
+  if (personIndex === -1) return res.status(404)
+  res.status(200).json(users[personIndex])
+})
+app.post("/person", (req, res) => {
+  const result = validatePerson(req.body)
+  if (!result.success) return res.status(422)
+
+  const newPerson = {
+    id: crypto.randomUUID(),
+    updated_date_at: new Date().toString("yyyy-MM-dd"),
+    updated_time_at: new Date().toString("HH:mm:ss"),
+    ...req.body
+  }
+  users.push(newPerson)
+  res.status(201)
+})
+app.patch("/person/:id", (req, res) => {
+  const { id } = req.params
+  const personIndex = users.findIndex(user => user.id === id)
+  if (personIndex === -1) return res.status(404)
+  const result = validatePartialPerson(req.body)
+  if (!result.success) return res.status(422)
+
+  const updatedPerson = {
+    ...req.body
+  }
+  users.push(updatedPerson)
+  res.status(200)
+})
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PROTOCOL}://${HOST}:${PORT}`)
